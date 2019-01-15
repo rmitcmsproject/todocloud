fc86df5542f6fb9fc0e13bddb4cb67fadd4f7174
rmitcmsproject <41687051+rmitcmsproject@users.noreply.github.com>
2019-01-15 14:53:56 +1100

    table login added

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ express()
     .get('/db', async (req, res) => {
         try {
             const client = await pool.connect()
-            const result = await client.query('SELECT * FROM test_table');
+            const result = await client.query('SELECT * FROM login');
             const results = {
                 'results': (result) ? result.rows : null
             };
